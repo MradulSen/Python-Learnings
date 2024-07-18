807077eef277cbf48a58bc3687af74801b7f2a3b
mradulpython <166733239+mradulpython@users.noreply.github.com>
2024-07-18 21:14:04 +0530

    Basic Datatypes

--- datatypes.py
+++ datatypes.py
@@ -1,3 +1,12 @@
-apples = 20 
+apples = 20 #int
+marks = 75.5 #float
+
+name = "mradul"  #string
+
+is_raining = False  # boolean
 
 print(type(apples))
+print(type(marks))
+print(type(name))
+print(type(is_raining))
+
