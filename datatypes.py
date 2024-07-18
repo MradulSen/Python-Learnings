93cb889810bef1d1269db648fea49f55cadd6e7d
mradulpython <166733239+mradulpython@users.noreply.github.com>
2024-07-18 21:28:00 +0530

    typecasting

--- datatypes.py
+++ datatypes.py
@@ -5,8 +5,29 @@ name = "mradul"  #string
 
 is_raining = False  # boolean
 
-print(type(apples))
-print(type(marks))
-print(type(name))
-print(type(is_raining))
+#  print(type(apples))
+#  print(type(marks))
+#  print(type(name))
+#  print(type(is_raining))
 
+# integer
+
+a= 20
+b= 30
+
+c= a + b
+
+print(type(c))
+
+#  typecasting
+
+marks = 20
+marks_float = float(marks)  # explicit typecasting
+print(type(marks_float))
+
+num = 20.5 # float
+den = 5  # int
+
+div = num/den    # implicit typecasting
+
+print(type(div))
