53f15297dd17b15c72c5091e0baef99c16d62e7b
mradulpython <166733239+mradulpython@users.noreply.github.com>
2024-07-19 00:19:17 +0530

    List Operations

---
+++ List_Ops.py
@@ -0,0 +1,11 @@
+list_of_players = ["Virat","Rohit","Jadega","Rahul","Raina"]
+
+print(len(list_of_players))
+
+print(list_of_players[1:4])  # slicing
+
+for player in list_of_players:
+    print(player)
+
+for i in range (0,len(list_of_players)):
+    print(list_of_players[i])
