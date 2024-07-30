60086e3158909a5f2ebffa1d4ffb5edbc0ca5a77
mradulpython <166733239+mradulpython@users.noreply.github.com>
2024-07-30 21:12:26 +0530

    Linked List

--- Linked_list_ops.py
+++ Linked_list_ops.py
@@ -3,9 +3,27 @@ class Node:
         self.data = data
         self.nextval = None
 
-if __name__ == " __main__":
+class LinkedList:
+        def __init__(self):
+            self.head =  None 
+
+        def traverse(self):
+             current = self.head
+
+             while current is not None:
+                  print(current.data)
+                  current = current.nextval
+
+if __name__ == "__main__":
     n1 = Node(1)
     n2 = Node(2)
     n3 = Node(3)
 
     print(n1.data)
+    linked_list = LinkedList()
+    linked_list.head = n1
+    linked_list.head.nextval = n2
+    n2.nextval = n3
+    n3.nextval = None
+
+    linked_list.traverse()
